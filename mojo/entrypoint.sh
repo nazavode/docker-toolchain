097e452cd6d6fcb149a80bf599ebb4b80607a1ae
Federico Ficarelli <1379149+nazavode@users.noreply.github.com>
2025-04-08 09:54:17 +0200

    Drop mojo

--- mojo/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-. /mojo-venv/bin/activate
-export MOJO_PATH=$(modular config mojo.path)
-export MODULAR_HOME="${HOME}/.modular"
-export PATH="${MOJO_PATH}/bin:${PATH}"
-
-exec "$@"

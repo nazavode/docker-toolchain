bfd2b7f48e697a17e5b22c24f998dd281d068208
Federico Ficarelli <1379149+nazavode@users.noreply.github.com>
2026-01-24 19:26:50 +0100

    Use mkl in gcc14-rhel-ubi9

--- gcc/14-rhel-ubi9/entrypoint.sh
+++ gcc/14-rhel-ubi9/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+# WARNING: intel setvars scripts mess with a lot of things,
+# including our arguments. Save them for later.
+ARGS="$@"
+
 . /opt/rh/gcc-toolset-14/enable
+. /opt/intel/oneapi/mkl/latest/env/vars.sh
 export CC=gcc
 export CXX=g++
 export FC=gfortran
 
-exec "$@"
+exec "$ARGS"
